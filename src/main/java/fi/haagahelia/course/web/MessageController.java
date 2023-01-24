8760657560dae2e90f1143c9eb7095b6fae71455
Pellikka Minna <h01340@haaga-helia.fi>
2023-01-24 15:44:17 +0200

    SB2.# to SB3.0.2

---
+++ src/main/java/fi/haagahelia/course/web/MessageController.java
@@ -0,0 +1,31 @@
+package fi.haagahelia.course.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fi.haagahelia.course.domain.Message;
+import jakarta.validation.Valid;
+
+@Controller
+public class MessageController {
+
+    @RequestMapping(value="/hello", method=RequestMethod.GET)
+    public String greetingForm(Model model) {
+        model.addAttribute("message", new Message());
+        return "hello";
+    }
+
+    @RequestMapping(value="/hello", method=RequestMethod.POST)
+    public String greetingSubmit(@Valid Message msg, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+        	return "hello";
+        }
+        
+    	model.addAttribute("message", msg);
+        return "result";
+    }
+
+}
